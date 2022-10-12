ac3e3a8ed61a774864e2d5c12be896f58e0d90a7
Esu Gabriel <orionesu.dev@gmail.com>
2022-10-12 22:22:18 +0100

    Calculator App

---
+++ src/components/calculator/btnConfig.js
@@ -0,0 +1,100 @@
+export const BTN_ACTIONS = {
+    ADD: 'ADD',
+    THEME: 'THEME',
+    CALC: 'CALC',
+    DELETE: 'DELETE'
+}
+
+export const btns = [
+    {
+        display: '9',
+        action: BTN_ACTIONS.ADD,
+        class: ''
+    },
+    
+    
+    {
+        display: '8',
+        action: BTN_ACTIONS.ADD,
+        class: ''
+    },
+    {
+        display: '7',
+        action: BTN_ACTIONS.ADD,
+        class: ''
+    },
+    {
+        display: '/',
+        action: BTN_ACTIONS.ADD,
+        class: 'btn__op'
+    },
+
+    {
+        display: '6',
+        action: BTN_ACTIONS.ADD,
+        class: ''
+    },
+    {
+        display: '5',
+        action: BTN_ACTIONS.ADD,
+        class: ''
+    },
+    {
+        display: '4',
+        action: BTN_ACTIONS.ADD,
+        class: ''
+    },
+    {
+        display: 'x',
+        action: BTN_ACTIONS.ADD,
+        class: 'btn__op'
+    },
+
+    {
+        display: '3',
+        action: BTN_ACTIONS.ADD,
+        class: ''
+    },
+    {
+        display: '2',
+        action: BTN_ACTIONS.ADD,
+        class: ''
+    },
+    {
+        display: '1',
+        action: BTN_ACTIONS.ADD,
+        class: ''
+    },
+    {
+        display: '-',
+        action: BTN_ACTIONS.ADD,
+        class: 'btn__op'
+    },
+
+
+    {
+        display: '0',
+        action: BTN_ACTIONS.ADD,
+        class: ''
+    },
+    {
+        display: '.',
+        action: BTN_ACTIONS.ADD,
+        class: ''
+    },
+    {
+        display: '+',
+        action: BTN_ACTIONS.ADD,
+        class: 'btn__op'
+    },
+    {
+        display: 'C',
+        action: BTN_ACTIONS.DELETE,
+        class: 'btn__op'
+    },
+    {
+        display: '=',
+        action: BTN_ACTIONS.CALC,
+        class: 'btn__op'
+    }
+];
